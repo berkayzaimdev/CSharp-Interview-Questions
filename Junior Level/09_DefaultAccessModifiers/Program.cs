1e59c5707f9e7e3cfd663751c86108a3f18dac5f
berkayzaimdev <berkayzaimdev@gmail.com>
2024-03-17 21:02:19 +0300

    Added "Default Access Modifiers"

---
+++ Junior Level/09_DefaultAccessModifiers/Program.cs
@@ -0,0 +1,28 @@
+﻿//bu sınıf, namespace seviyesinde çağrıldığı için default olarak internal'dır. ya da public olabilir başka bir access modifier alamaz
+class ClassAtNamespaceLevel
+{
+    //bu parametre, default olarak private'tır
+    int number;
+
+    public int publicNumber;
+
+
+    //bu sınıf, sınıf seviyesinde çağrıldığı için default olarak private'tır
+    class InnerClass
+    {
+
+    }
+}
+
+public class PublicClassAtNamespaceLevel
+{
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello!");
+        Console.ReadKey();
+    }
+}
