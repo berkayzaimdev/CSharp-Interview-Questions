08aff96b731e7f8d2af6690cdebbabedbaf0f46f
berkayzaimdev <berkayzaimdev@gmail.com>
2024-03-17 23:39:55 +0300

    Added "New Keyword"

---
+++ Junior Level/14_NewKeyword/NewOperator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_NewKeyword
+{
+    class NewOperator
+    {
+        public static void CreateSomeObjects()
+        {
+            //new operator
+            var rachel = new Person("Rachel", 34); //constructor çağrıldı
+
+            Person john = new("John", 19); //C# 9 kullanımı
+
+            var steve = new Person { Name = "Steve", Age = 45 }; //nesne tanımlaması
+
+            var currencies = new Dictionary<string, string> //collection tanımlaması
+            {
+                ["USA"] = "USD",
+                ["Great Britain"] = "GBP"
+            };
+
+            var numbers = new int[4]; //dizi tanımlaması
+
+            var person = new { Name = "Anna", Age = 55 }; //sınıfsız tanımlama 
+        }
+    }
+
+    class Person
+    {
+        public string Name;
+        public int Age;
+
+        public Person()
+        {
+
+        }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+}
---
+++ Junior Level/14_NewKeyword/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * new keyword'ü 3 farklı şekilde karşımıza çıkar;
+ *    - 
+ */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var pet1 = new Pet("Hannibal");
+        var lazyList = new NewConstraint.LazyInitializer<List<int>>();
+        //Pet class doesn't have parameterless constructor
+        //var lazyPet = new NewConstraint.LazyInitializer<Pet>(); 
+
+        Console.ReadKey();
+    }
+}
+
+class NewConstraint
+{
+    //without this constraint, the "new T()" below would not work
+    public class LazyInitializer<T> where T : new() // new modifier ile T olarak kullanmak istediğimiz nesnenin parametresiz bir constructor'a sahip olmasını zorunlu kılarız
+    {
+        private T value;
+
+        public T Get()
+        {
+            if (value == null)
+            {
+                value = new T();
+            }
+            return value;
+        }
+    }
+}
+
+class Pet
+{
+    string Name;
+
+    public Pet(string name)
+    {
+        Name = name;
+    }
+}
