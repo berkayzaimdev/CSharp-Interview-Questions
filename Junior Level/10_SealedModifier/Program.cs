e77c7499fdb5cbb236c8394b6a7b4fd5fcaa50ed
berkayzaimdev <berkayzaimdev@gmail.com>
2024-03-17 21:04:04 +0300

    Added "Sealed Modifier"

---
+++ Junior Level/10_SealedModifier/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+public class Base
+{
+    public virtual void DoSomething()
+    {
+        Console.WriteLine("Base class");
+    }
+}
+
+public class Derived : Base
+{
+    public override sealed void DoSomething() //sealed ile işaretlenen bir method, override edilemez
+    {
+        Console.WriteLine("Derived class");
+    }
+}
+
+public class DerivedFromDerived : Derived
+{
+    //bu kod bloğu derlenemez çünkü bir üst sınıfta bu metodu sealed olarak işaretlemiştik
+    //public override void DoSomething() 
+    //{
+    //}
+}
+
+public sealed class SealedBase
+{
+
+}
+
+//bu sınıf derlenemez çünkü bir üst sınıfı sealed olarak işaretlemiştik
+//public class DerivedFromSealed : SealedBase
+//{
+//}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello!");
+        Console.ReadKey();
+    }
+}
