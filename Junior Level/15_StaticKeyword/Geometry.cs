1c66cadc872219de843e6e737ae10f7c733f9328
berkayzaimdev <berkayzaimdev@gmail.com>
2024-03-17 23:46:11 +0300

    Added "Static Keyword"

---
+++ Junior Level/15_StaticKeyword/Geometry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_StaticKeyword
+{
+    static class Geometry
+    {
+        //public static double SquareArea(double side)
+        //{
+        //    return Pow(side, 2);
+        //}
+
+        //public static double CircleArea(double radius)
+        //{
+        //    return PI * Pow(radius, 2);
+        //}
+
+        //public static double EquilateralTriangleArea(double side)
+        //{
+        //    return (Pow(side, 2) * Sqrt(3)) / 4d;
+        //}
+    }
+}
---
+++ Junior Level/15_StaticKeyword/Program.cs
@@ -0,0 +1,41 @@
+﻿class Box
+{
+    public static int MaxCount = 50;
+
+    private List<string> elements = new List<string>();
+
+    public void Add(string element)
+    {
+        if (elements.Count < MaxCount)
+        {
+            elements.Add(element);
+        }
+    }
+
+    //bu metodu static olarak işaretleyemeyiz çünkü dönüş değeri static olmayan bir ifade barındırıyor
+    public int GetCurrentCount()
+    {
+        return elements.Count;
+    }
+
+    public static string FormatMaxCount()
+    {
+        return $"The max count for this Box is {MaxCount}";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var box1 = new Box();
+        var box2 = new Box();
+        //var invalidMaxCount = box1.MaxCount; //static parametreyi kullanabilmek için instance değil tip parametresinden faydalanırız
+        var maxCount = Box.MaxCount;
+        box2.Add("s");
+        var elementsCount = box2.GetCurrentCount();
+        var maxCountFormatted = Box.FormatMaxCount();
+
+        Console.ReadKey();
+    }
+}
