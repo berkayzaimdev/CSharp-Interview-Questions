aaf7b3ccae9fdb6e513d2a774e104008509a126f
berkayzaimdev <berkayzaimdev@gmail.com>
2024-03-17 21:38:36 +0300

    Added "Class vs Struct"

---
+++ Junior Level/12_ClassVsStruct/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+struct Point
+{
+    public int x;
+    public int y;
+
+    //derlenemez çünkü tek bir constructor olabilir ve o constructor tüm parametreleri içermelidir
+    //public Point()
+    //{
+
+    //}
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello!");
+        Console.ReadKey();
+    }
+}
+
+/* 
+ * Struct'lar hangi durumlarda kullanılmalıdır?
+ *    - Tip, minimal boyuttaysa
+ *    - Tip, değiştirilemez ise
+ *    - Tip, kısa ömürle kullanılacak ise
+ *    - Tip, diğer nesnelerle etkileşimde olacaksa
+
+
+  
+ * Struct vs Class,
+ *    - Structlar değer tipidir, class ise referans tipidir
+ *    - Struct tek bir constructor içerir
+ *    - Struct, destructor içeremez
+*/
