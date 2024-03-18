81836713e4b6a2538b9d8a9fca144ee1231006ff
berkayzaimdev <berkayzaimdev@gmail.com>
2024-03-18 16:20:38 +0300

    Added "Method Overriding vs. Method Hiding"

---
+++ Junior Level/35_MethodOverridingVsMethodHiding/Shapes.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _35_MethodOverridingVsMethodHiding
+{
+    public class Circle //imagine this is defined in an external library that you use
+    {
+        public double Radius { get; }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Circumference()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public virtual string Draw()
+        {
+            return @"O";
+        }
+    }
+
+    public class SmartCircle : Circle
+    {
+        public SmartCircle(double radius) : base(radius)
+        {
+        }
+
+        public double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public new string Draw()
+        {
+            return @"
+                   *  *    
+                *        * 
+               *          *
+               *          *
+                *        * 
+                   *  *    ";
+        }
+    }
+}
