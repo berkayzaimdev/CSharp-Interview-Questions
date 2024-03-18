733aeed1b4290e12e79c970e698acbbe4deba2d2
berkayzaimdev <berkayzaimdev@gmail.com>
2024-03-18 17:19:40 +0300

    Added "Bridge"

---
+++ Junior Level/44_Bridge/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+
+class Car
+{
+    public Motor Motor { get; }
+    public Gear Gear { get; }
+
+    public Car(Motor motor, Gear gear)
+    {
+        Motor = motor;
+        Gear = gear;
+    }
+}
+
+class Pickup : Car
+{
+    public Pickup(Motor motor, Gear gear) : base(motor, gear)
+    {
+    }
+}
+
+class Sedan : Car
+{
+    public Sedan(Motor motor, Gear gear) : base(motor, gear)
+    {
+    }
+}
+
+class Motor { }
+class ElectricMotor : Motor { }
+class PetrolMotor : Motor { }
+
+class Gear { }
+class ManualGear : Gear { }
+class AutomaticGear : Gear { }
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var electricPickupWithManualGear = new Pickup(new ElectricMotor(), new ManualGear());
+        var petrolSedanWithAutomaticGear = new Sedan(new PetrolMotor(), new AutomaticGear());
+
+        Console.WriteLine("Hello!");
+        Console.ReadKey();
+    }
+}
+
+// Bunu Bridge pattern kullanmadan tasarlamayı deneseydik, çok fazla olasılık elde ederdik. Bunu önleyebilmek için Motor ve Gear'ı iki ayrı entity olarak tanımladık
