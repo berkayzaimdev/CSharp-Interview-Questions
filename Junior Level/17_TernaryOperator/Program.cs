950811a6114c3d1f2ed2a6816e35fd2e8542950f
berkayzaimdev <berkayzaimdev@gmail.com>
2024-03-18 09:37:12 +0300

    Added "Ternary Operator"

---
+++ Junior Level/17_TernaryOperator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var dog = new Dog(15);
+
+        //string size;
+        //if (dog.Weight > 25)
+        //{
+        //    size = "big";
+        //}
+        //else
+        //{
+        //    size = "small";
+        //}
+
+        var size = dog.Weight > 25 ? "big" : "small";
+
+        Console.WriteLine($"The dog is {size}");
+
+        var moreSpecificSize = dog.Weight > 25 ? "big" : dog.Weight < 5 ? "small" : "medium";
+
+        Console.WriteLine($"The dog is {moreSpecificSize}");
+
+        //bu kod bloğu çalışmaz çünkü ternary operator'de mutlaka bir atama olmalıdır. Atama dışında return keyword ile de kullanımı mümkündür
+        //dog.Weight > 100 ? 
+        //    Console.WriteLine("Wow!") : 
+        //    Console.WriteLine("It's a normal dog");
+
+        Console.ReadKey();
+    }
+}
+
+class Dog
+{
+    public int Weight;
+
+    public Dog(int weight)
+    {
+        Weight = weight;
+    }
+}
